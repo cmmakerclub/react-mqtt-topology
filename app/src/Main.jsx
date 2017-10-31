ed9480266a3ce087251ace88e90f653ecd255b21
tan <nat@cmmc.io>
2017-10-31 15:53:38 +0700

    auto format.

--- app/src/Main.jsx
+++ app/src/Main.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Info from './components/Info.jsx'
-import List from './components/List.jsx'
-import Child from './components/Child.jsx'
-import Topology from './components/Topology.jsx'
+import Info from './components/Info.jsx';
+import List from './components/List.jsx';
+import Child from './components/Child.jsx';
+import Topology from './components/Topology.jsx';
 
-const nodesId = {}
-const edgesId = {}
+const nodesId = {};
+const edgesId = {};
 const multipleMaster = {};
 
 class Container extends React.Component {
 
   constructor (props) {
-    super(props)
-    this.state = {objLists: []}
+    super(props);
+    this.state = {objLists: []};
   }
 
   componentDidMount () {
-    // create an array with nodes
-    window.nodes = new vis.DataSet([])
-    // create an array with edges
-    window.edges = new vis.DataSet([])
+    window.nodes = new vis.DataSet([]);
+    window.edges = new vis.DataSet([]);
 
     window.fn = () => {
-
       Object.keys(objMaster).forEach((key, idx) => {
         // console.log(`${key}`)
         // console.log(`debug : ${this.state.objLists}`)
@@ -32,57 +29,55 @@ class Container extends React.Component {
         if (nodesId[key] === undefined) {
           nodes.add({
             id: key, label: key
-          })
-          multipleMaster[key] = (<List header={key}/>) // create master
-          nodesId[key] = 1
+          });
+          multipleMaster[key] = (<List header={key}/>); // create master
+          nodesId[key] = 1;
         }
         objMaster[key].children_array.forEach((v, idx) => {
             if (nodesId[v.info.from] === undefined) {
               nodes.add({
                 id: v.info.from, label: `--${v.info.from}`
-              })
+              });
 
               //multipleMaster[key].child[key] = (<Child name={`${v.info.from}`}/>) // create child
 
-              nodesId[v.info.from] = 1
+              nodesId[v.info.from] = 1;
             }
             if (!edgesId[`${v.info.from}-${v.info.to}`]) {
               edges.add({
                 from: v.info.from, to: v.info.to, arrows: 'to', dashes: false
-              })
-              edgesId[`${v.info.from}-${v.info.to}`] = 1
+              });
+              edgesId[`${v.info.from}-${v.info.to}`] = 1;
             }
           }
-        )
+        );
         // window.network.stabilize()
 
         _.forOwn(multipleMaster, (value, key) => {
-
           console.log(key);
+        });
 
-        })
+        ReactDOM.render(multipleMaster, document.getElementById('list-master'));
+        ReactDOM.render(multipleChild, document.getElementById('list-child'));
 
-        ReactDOM.render(multipleMaster, document.getElementById('list-master'))
-        ReactDOM.render(multipleChild, document.getElementById('list-child'))
+      });
 
-      })
-
-    }
+    };
 
     // create a network
-    let container = document.getElementById('mynetwork')
+    let container = document.getElementById('mynetwork');
     let data = {
       nodes: nodes,
       edges: edges
-    }
-    let options = {}
-    window.network = new vis.Network(container, data, options)
+    };
+    let options = {};
+    window.network = new vis.Network(container, data, options);
 
     // setTimeout(fn, 5000)
     setInterval(() => {
-      fn()
+      fn();
       // network.stabilize()
-    }, 1000)
+    }, 1000);
 
   }
 
@@ -108,8 +103,8 @@ class Container extends React.Component {
 
 
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<Container/>, document.getElementById('app'))
+ReactDOM.render(<Container/>, document.getElementById('app'));
